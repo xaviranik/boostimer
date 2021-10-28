e12fecab8759d3f5213bb963db5bb9ddebbd6106
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2021-10-28 20:15:54 +0600

    chore: admin class refactor

--- includes/Admin.php
+++ includes/Admin.php
@@ -18,17 +18,45 @@ class Admin {
      * @since 1.0.0
      */
     public function __construct() {
+        $this->initialize();
+        $this->set_hooks();
+    }
+
+    /**
+     * Initiates admin classes
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public function initialize() {
         // Asset Loader
         new Assets();
         // Admin menu manager
         new Menu();
         // Product data manager
         new ProductData();
+    }
 
+    /**
+     * Sets up hooks.
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
+    public function set_hooks() {
         // Plugin page cleanup
         add_action( 'admin_head', [ $this, 'cleanup_admin_notices' ], 1 );
     }
 
+    /**
+     * Cleans admin notice for woo-availability page
+     *
+     * @since 1.0.0
+     *
+     * @return void
+     */
     public function cleanup_admin_notices() {
         global $pagenow;
 
