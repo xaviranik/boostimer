5a2cfb903e4303d5b690dfd3abf89cba8460f574
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2021-10-28 20:08:02 +0600

    add: admin panel dashboard cleanup

--- includes/Admin.php
+++ includes/Admin.php
@@ -24,5 +24,18 @@ class Admin {
         new Menu();
         // Product data manager
         new ProductData();
+
+        // Plugin page cleanup
+        add_action( 'admin_head', [ $this, 'cleanup_admin_notices' ], 1 );
+    }
+
+    public function cleanup_admin_notices() {
+        global $pagenow;
+
+        if ( ! ( $pagenow == 'admin.php' && $_GET['page'] == 'woo-availability' ) ) {
+            return;
+        }
+
+        remove_all_actions( 'admin_notices' );
     }
 }
