1d4b393d1992fe0662b3201b84f741a3b8999e97
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2021-10-30 09:45:24 +0600

    chore: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const fs = require('fs');
-const path = require('path');
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const fs = require("fs");
+const path = require("path");
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const DependencyExtractionWebpackPlugin = require("@wordpress/dependency-extraction-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
-const PROD = process.env.NODE_ENV === 'production';
+const PROD = process.env.NODE_ENV === "production";
 
 const coreModule = {
   ...defaultConfig,
   entry: {
-    core: './src/index.js',
+    core: "./src/index.js",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: PROD ? '[name].min.js' : '[name].js',
-    publicPath: '/wp-content/plugins/woo-availability/dist/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/wp-content/plugins/woo-availability/dist/",
     // chunkFilename: 'chunks/[name].js',
   },
   resolve: {
     alias: {
       ...defaultConfig.resolve.alias,
-      '@': path.resolve('./src/'),
+      "@": path.resolve("./src/"),
     },
   },
   plugins: [
     // remove the plugin, next insert our own logic
     ...defaultConfig.plugins.filter(
       (plugin) =>
-        plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+        plugin.constructor.name !== "DependencyExtractionWebpackPlugin"
     ),
     new DependencyExtractionWebpackPlugin({
       injectPolyfill: true,
       requestToExternal(request) {
         // remove WordPress externals, except...
         // api-fetch, because it sets the API fetching defaults
-        if (request !== '@wordpress/api-fetch') {
-          return '';
+        if (request !== "@wordpress/api-fetch") {
+          return "";
         }
       },
     }),
@@ -51,8 +51,8 @@ const coreModule = {
         ...defaultConfig.optimization.splitChunks.cacheGroups,
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+          name: "vendors",
+          chunks: "all",
         },
       },
     },
@@ -60,15 +60,15 @@ const coreModule = {
 };
 
 const mainModule = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   entry: {
-    'js/admin': './assets/src/js/admin.js',
-    'js/frontend': './assets/src/js/frontend.js',
-    'css/frontend': './assets/src/scss/frontend.scss',
+    "js/admin": "./assets/src/js/admin.js",
+    "js/frontend": "./assets/src/js/frontend.js",
+    "css/frontend": "./assets/src/scss/frontend.scss",
   },
   output: {
-    path: path.resolve(__dirname, 'assets'),
-    filename: PROD ? '[name].min.js' : '[name].js',
+    path: path.resolve(__dirname, "assets"),
+    filename: PROD ? "[name].min.js" : "[name].js",
   },
   optimization: {
     minimize: PROD,
@@ -81,19 +81,18 @@ const mainModule = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'file-loader',
-            options: { outputPath: 'css/', name: PROD ? '[name].min.css' : '[name].css'}
+            loader: "file-loader",
+            options: {
+              outputPath: "css/",
+              name: PROD ? "[name].min.css" : "[name].css",
+            },
           },
-          'sass-loader'
-        ]
-      }
-    ]
+          "sass-loader",
+        ],
+      },
+    ],
   },
-  plugins: [
-    new FixStyleOnlyEntriesPlugin(),
-  ],
+  plugins: [new FixStyleOnlyEntriesPlugin()],
 };
 
-module.exports = [
-  coreModule, mainModule
-];
+module.exports = [coreModule, mainModule];
