37a496c60e4a7441e293fd8083a444a5d35df427
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2021-11-01 23:38:02 +0600

    chore: zip build sh script

---
+++ bin/build.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Exit if any command fails.
+set -e
+
+# Change to the expected directory.
+cd "$(dirname "$0")"
+cd ..
+DIR=$(pwd)
+BUILD_DIR="$DIR/build/woo-availability"
+
+# Enable nicer messaging for build status.
+BLUE_BOLD='\033[1;34m'
+GREEN_BOLD='\033[1;32m'
+RED_BOLD='\033[1;31m'
+YELLOW_BOLD='\033[1;33m'
+COLOR_RESET='\033[0m'
+
+error() {
+    echo -e "\n${RED_BOLD}$1${COLOR_RESET}\n"
+}
+status() {
+    echo -e "\n${BLUE_BOLD}$1${COLOR_RESET}\n"
+}
+success() {
+    echo -e "\n${GREEN_BOLD}$1${COLOR_RESET}\n"
+}
+warning() {
+    echo -e "\n${YELLOW_BOLD}$1${COLOR_RESET}\n"
+}
+
+status "Building woo-availability zip file..."
+
+# remove the build directory if exists and create one
+rm -rf "$DIR/build"
+mkdir -p "$BUILD_DIR"
+
+# Run the build.
+status "Installing npm dependencies... 📦"
+# npm install
+
+status "Generating build... 👷‍♀️"
+yarn build
+yarn makepot
+yarn pot2json
+yarn readme
+
+# Copy all files
+status "Copying files... ✌️"
+FILES=(woo-availability.php readme.txt dist includes assets templates languages composer.json composer.lock LICENCE)
+
+for file in ${FILES[@]}; do
+    cp -R $file $BUILD_DIR
+done
+
+# Install composer dependencies
+status "Installing dependencies... 📦"
+cd $BUILD_DIR
+composer install --optimize-autoloader --no-dev -q
+
+# Remove composer files
+rm composer.json composer.lock
+
+# go one up, to the build dir
+status "Creating archive... 🎁"
+cd ..
+zip -r -q woo-availability.zip woo-availability
+
+# remove the source directory
+rm -rf woo-availability
+
+success "Done. You've built woo-availability! 🎉 "
